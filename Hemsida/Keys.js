29bdbca9af8eb2567a87d2fb5d13f6dc48faf621
HarleyBerglund <55533374+HarleyBerglund@users.noreply.github.com>
2020-03-24 11:55:06 +0100

    Add files via upload

---
+++ Hemsida/Keys.js
@@ -0,0 +1,54 @@
+
+document.addEventListener('keypress', function (event) {
+    if (event.defaultPrevented) {
+        return;
+    }
+    var key = event.key || event.keyCode;
+
+    if (key === 'w' || key === 87) {
+        POWER = 0;
+        output = 1023;
+        onMotor();
+
+    }
+    if (key === 's' || key === 83) {
+        POWER = 1;
+        output = 1023;
+        onMotor();
+    }
+
+    if (key === 'a' || key === 65) {
+        output2 = 0;
+        onServo();
+        document.addEventListener("keyup", keyUP);
+    }
+    if (key === 'd' || key === 68) {
+        output2 = 2;
+        onServo();
+        document.addEventListener("keyup", keyUP);
+    }
+})
+
+document.addEventListener('keyup', function (event) {
+    if (event.defaultPrevented) {
+        return;
+    }
+    var key = event.key || event.keyCode;
+
+    if (key === 'w' || key === 87) {
+        POWER = 0;
+        output = 0;
+        onMotor();
+
+    }
+    if (key === 's' || key === 83) {
+        POWER = 1;
+        output = 0;
+        onMotor();
+    }
+})
+
+function keyUP() {
+    output2 = 1;
+    onServo();
+}
---
+++ MQTT_MOTOR/MQTT_MOTOR.ino
@@ -0,0 +1,61 @@
+//including libraries
+#include "EspMQTTClient.h"
+#include <ESP8266WiFi.h>
+#include <Servo.h>
+
+//Defining pins
+#define motorPinRightDir  0//D3
+#define motorPinRightSpeed 5//D1
+
+void onConnectionEstablished();
+
+Servo My_servo;
+
+EspMQTTClient client(
+ "Berglund",           // Wifi ssid  ABB_Indgym_Guest
+  "71032104107103210410710321",               // Wifi password   Welcome2abb
+  "maqiatto.com",              // MQTT broker ip
+  1883,                        // MQTT broker port
+  "harley@berglun.se",         // MQTT username
+  "HEJHEJ",                    // MQTT password
+  "Node",                      // Client name
+  onConnectionEstablished,     // Connection established callback
+  true,                        // Enable web updater
+  true                         // Enable debug messages
+);
+
+void setup(){
+  pinMode(LED_BUILTIN, OUTPUT);
+  analogWrite(LED_BUILTIN, 0);
+  Serial.begin(115200);
+  pinMode(motorPinRightDir, OUTPUT);
+  pinMode(motorPinRightSpeed, OUTPUT);
+  My_servo.attach(2); //D4
+  My_servo.write(90);
+}
+
+void onConnectionEstablished(){
+  client.subscribe("harley@berglun.se/motor", [] (const String &payload1){    
+    int Power = payload1.substring(1, payload1.indexOf(',')).toInt();
+    int Strength = payload1.substring(payload1.lastIndexOf(',') + 1).toInt();   
+
+    Serial.println(Strength);
+    Serial.println(Power);
+    digitalWrite(motorPinRightDir, Power);
+    analogWrite(motorPinRightSpeed, Strength);
+  });
+
+    client.subscribe("harley@berglun.se/servo", [] (const String &payload2){    
+    int SERVO = payload2.substring(1, payload2.indexOf(',')).toInt();
+
+    SERVO = map(SERVO, 0, 2, 170, 10);
+
+    Serial.println(SERVO);
+    My_servo.write(SERVO);
+  });
+  }
+
+
+void loop(){
+    client.loop();
+}
