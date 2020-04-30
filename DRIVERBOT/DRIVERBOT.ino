0a7e9df7479fb640650d2559e532874923b1f075
HarleyBerglund <55533374+HarleyBerglund@users.noreply.github.com>
2020-04-30 14:25:56 +0200

    hkjbkivv

--- DRIVERBOT/DRIVERBOT.ino
+++ DRIVERBOT/DRIVERBOT.ino
@@ -2,13 +2,12 @@
 #include <EspMQTTClient.h>
 #include <ESP8266WiFi.h>
 #include <Servo.h>
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
 
 #include "OLED_SCREEN.h"
 #include "CREDENTIALS.h"
 #include "RECEIVE.h"
+
+unsigned long time_now;
 
 void onConnectionEstablished();
 
@@ -20,22 +19,16 @@ void setup() {
   pinMode(motorPinRightSpeed, OUTPUT);
   My_servo.attach(2); //D4
   My_servo.write(90);
-
-  //...................OLED_SCREEN........................................
-  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
-    Serial.println(F("SSD1306 allocation failed"));
-    for (;;);
-  }
-  delay(2000); // Pause for 2 seconds
-
-  // Clear the buffer.
-  display.clearDisplay();
-
-  // Draw bitmap on the screen
-  display.drawBitmap(0, 0, image_data_Saraarray, 128, 64, 1);
-  display.display();
+  
 }
+
 
 void loop() {
   client.loop();
+
+  if (millis() > time_now + 2000){
+   oledScreen();                                     //Looping the getTemperature every 2 seconds
+   time_now = millis();
+}
+
 }
--- DRIVERBOT/RECEIVE.h
+++ DRIVERBOT/RECEIVE.h
@@ -1,5 +1,16 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
 #define motorPinRightDir  0//D3
 #define motorPinRightSpeed 5//D1
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+
+int Strength;
 
 Servo My_servo;
 
@@ -20,4 +31,22 @@ void onConnectionEstablished() {
     //Serial.println(SERVO);
     My_servo.write(SERVO);
   });
+}
+
+
+void oledScreen() {
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+  delay(2000);
+  display.clearDisplay();
+
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 20);
+  // Display static text
+  display.println(Strength);
+  display.display(); 
 }
